651e019525adc8bbd447977d13dcf8c3eb858c2c
Bas van Kuijck <bas@e-sites.nl>
2020-09-03 13:55:27 +0200

    1.4.2

--- Package.swift
+++ Package.swift
@@ -4,11 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "Radon",
+    platforms: [
+        .macOS(.v10_13),
+    ],
     products: [
         .executable(name: "radon", targets: ["Radon"])
     ],
     dependencies: [
-        .package(url: "https://github.com/basvankuijck/CommandLine.git", from: "4.0.0"),
+        .package(url: "https://github.com/basvankuijck/CommandLine.git", .branch("master")),
         .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.0"),
         .package(url: "https://github.com/e-sites/Francium.git", from: "1.0.0")
     ],
